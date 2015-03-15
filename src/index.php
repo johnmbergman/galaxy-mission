17b6741813bb4255bb80f24f5777287c67c929af
John Bergman <johnmbergman@gmail.com>
2015-03-14 23:52:40 -0500

    Removed white text, added global js file to index

--- src/index.php
+++ src/index.php
@@ -122,6 +122,7 @@
   <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script src="/js/bootstrap.min.js"></script>
+  <script src="/js/global.js"></script>
 
   <!-- Load the javascript file for the page if applicable -->
   <?php
