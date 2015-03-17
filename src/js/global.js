f751a6a75f6b4121fb8f49092af04a4d9203228d
Adam Hill <ahill32605@gmail.com>
2015-03-16 22:08:54 -0400

    uiMessageBox
    Function added to global.js

--- src/js/global.js
+++ src/js/global.js
@@ -3,12 +3,57 @@ Scrum of the Earth
 Global Javascript functions for Galaxy Mission
 */ 
 
-// Function that takes string and returns popup output for character
-// Adam Hill 3/10/15
-function characterSpeak(string)
-{
-	return BootstrapDialogue.show({
-		title: '{Character's name} says:',
-		message: string
-		});
+
+<script>
+
+function uiMessageBox(uiTitle, uiMessage, imgSrc) {
+/* 
+|--------------------------------------------------------------|
+|  Customized message box                                      | 
+|  Takes string for title bar and body of Modal Dialog box and |
+|  source location of image to be displayed in box             |
+|  John Bergman - Adam Hill 3/16/15                            |
+|--------------------------------------------------------------|
+*/
+
+   try {
+      // Set the message
+      var tempMessageBox = $("<div>").attr("class", "modal fade").append(
+            $("<div>").attr("class", "modal-dialog").append(
+               $("<div>").attr("class", "modal-content").append(
+
+                  $("<div>").attr("class", "modal-header").append(
+                     $("<h3>").attr("class", "modal-title").text(uiTitle)
+                  )
+
+
+               ).append(
+
+                  $("<div>").attr("class", "modal-body").append(
+                     $("<h3>").html(imgSrc).append(
+                     $("<h3>").text(uiMessage))
+                  )
+                  
+
+               ).append(
+
+                  $("<div>").attr("class", "modal-footer").append(
+                     $("<button>").attr("type", "button").attr("class", "btn btn-primary").attr("data-dismiss", "modal").text("OK")
+                  )
+
+               )
+            )
+         ).on("hidden.bs.modal", function () {
+            $(this).remove();
+         }).modal("show");
+
+         $("body").append(tempMessageBox);
+         tempMessageBox.modal("show");
+
+   } catch (ex) {
+      alert(uiMessage);
+   }
+
 }
+</script>
+
