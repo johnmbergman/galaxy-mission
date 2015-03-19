d7affdd6436b60c98ba54a630cd3c6e1a1062f3c
Adam Hill <ahill32605@gmail.com>
2015-03-18 20:35:13 -0400

    Updated uiMessageBox
    Added condition for presence of image argument.

--- src/js/global.js
+++ src/js/global.js
@@ -14,7 +14,9 @@ Global Javascript functions for Galaxy Mission
 function uiMessageBox(uiTitle, uiMessage, imgSrc) {
 
    try {
-      // Set the message
+   	if (imgSrc.length > 0)
+   	{
+      // Set the message with image
       var tempMessageBox = $("<div>").attr("class", "modal fade").append(
             $("<div>").attr("class", "modal-dialog").append(
                $("<div>").attr("class", "modal-content").append(
@@ -27,7 +29,7 @@ function uiMessageBox(uiTitle, uiMessage, imgSrc) {
                ).append(
 
                   $("<div>").attr("class", "modal-body").append(
-                     $("<img>").attr("src", "../res/" + imgSrc).append(
+                     $("<img>").attr("src", "../res/" + imgSrc +).append(
                      $("<p>").text(uiMessage))
                   )
                   
@@ -43,7 +45,37 @@ function uiMessageBox(uiTitle, uiMessage, imgSrc) {
          ).on("hidden.bs.modal", function () {
             $(this).remove();
          }).modal("show");
+	}
+	else
+	{
+	 // Set the message without image
+      var tempMessageBox = $("<div>").attr("class", "modal fade").append(
+            $("<div>").attr("class", "modal-dialog").append(
+               $("<div>").attr("class", "modal-content").append(
 
+                  $("<div>").attr("class", "modal-header").append(
+                     $("<h4>").attr("class", "modal-title").text(uiTitle)
+                  )
+
+               ).append(
+
+                  $("<div>").attr("class", "modal-body").append(
+                     $("<p>").text(uiMessage)
+                  )
+
+               ).append(
+
+                  $("<div>").attr("class", "modal-footer").append(
+                     $("<button>").attr("type", "button").attr("class", "btn btn-primary").attr("data-dismiss", "modal").text("OK")
+                  )
+
+               )
+            )
+         ).on("hidden.bs.modal", function () {
+            $(this).remove();
+         }).modal("show");
+     }
+     
          $("body").append(tempMessageBox);
          tempMessageBox.modal("show");
 
