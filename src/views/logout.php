e438c2469df1e0436a27e9afb2c9dd1a39d5db74
John Bergman <johnmbergman@gmail.com>
2015-03-22 17:50:16 -0500

    Added logout view

---
+++ src/views/logout.php
@@ -0,0 +1,15 @@
+<div class="row">
+  <div class="col-md-12">
+    <div class="text-center">
+      <h1>
+        <i class="fa fa-sign-out fa-5x"></i>
+      </h1>
+      <h1>
+        Signed out
+      </h1>
+      <p class="lead">
+        You have been signed out of your account. <a href="/login/">Click here</a> to sign back in.
+      </p>
+    </div>
+  </div>
+</div>
