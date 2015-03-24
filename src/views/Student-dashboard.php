8948a3efb685b6c1725bcdf7c5a92aeadf0570cb
Adam Hill <ahill32605@gmail.com>
2015-03-24 00:22:26 -0400

    Creating student dashboard view
    Created html for student dashboard with placeholder images and buttons.

---
+++ src/views/Student-dashboard.php
@@ -0,0 +1,98 @@
+<?php
+
+?>
+<head>
+<style>
+div.padded{
+  padding-top: 40px;
+  padding-bottom: 40px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+img{
+	width:100px;
+	height:100px;
+	margin: 10px 10px;
+	}
+
+</style>
+</head>
+
+
+<div class="row">
+  <div class="col-md-6 col-md-offset-3">
+    <div class="text-center" color=white>
+      <h1>Student Dashboard</h1>
+    </div>
+  </div>
+</div>
+<div class="row" style="margin-top:20px">
+ <!-- <div class="col-sm-1">
+  </div>-->
+  <div class="col-sm-10 col-sm-offset-1" >
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="panel-primary">
+          <div class="panel-heading">
+            <h2 class="panel-title text-center">Training</h2>
+          </div>
+          <div class="panel-body">
+            <div class="row text-center">
+              <div class="col-md-6 col-md-offset-3">
+                <img src="../res/earth.png" alt="earth">
+	            <a href="#" class="btn btn-success">Assignment #1</a>
+              </div>
+            </div>
+            <div class="row text-center">
+              <div class="col-md-4">
+                <img src="../res/jupiter.png" alt="jupiter">
+	            <a href="#" class="btn btn-info">Assignment #2</a>
+              </div>
+             <div class="col-md-4 col-md-offset-4">
+                <img src="../res/mars.png" alt="mars">
+	            <a href="#" class="btn btn-warning">Assignment #3</a>
+              </div>
+            </div>
+            <div class="row text-center">
+              <div class="col-md-6 col-md-offset-3">
+                <img src="../res/neptune.png" alt="neptune">
+	            <a href="#" class="btn btn-danger">Assignment #4</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="panel-primary">
+          <div class="panel-heading">
+            <h2 class="panel-title text-center">Missions</h2>
+          </div>
+          <div class="panel-body">
+            <div class="row text-center">
+              <div class="col-md-6 col-md-offset-3">
+                <img src="../res/earth.png" alt="earth">
+              </div>
+              <div class="col-md-6 col-md-offset-3">
+	            <a href="#" class="btn btn-success">Mission #1</a>
+              </div>
+            </div>
+            <div class="row text-center">
+              <div class="col-md-4">
+                <img src="../res/jupiter.png" alt="jupiter">
+	            <a href="#" class="btn btn-info">Mission #2</a>
+              </div>
+             <div class="col-md-4 col-md-offset-4">
+                <img src="../res/mars.png" alt="mars">
+	            <a href="#" class="btn btn-warning">Mission #3</a>
+              </div>
+            </div>
+            <div class="row text-center">
+              <div class="col-md-6 col-md-offset-3">
+                <img src="../res/neptune.png" alt="neptune">
+              </div>
+              <div class="col-md-6 col-md-offset-3">  
+	            <a href="#" class="btn btn-danger">Mission #4</a>
+              </div>
+            </div>
+          </div>
+        </div>    
