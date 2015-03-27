f838613097d6e2ac9c49cfb93cf587f9dfc83426
Adam Hill <ahill32605@gmail.com>
2015-03-27 15:31:21 -0400

    Re-designed student-dashboard
    I redesigned the look using a panel for each level or module containing
    a list of the available missions for that module.

--- src/views/Student-dashboard.php
+++ src/views/Student-dashboard.php
@@ -1,129 +1,160 @@
-<head>
-<style>
 
-div.padded{
- 
-  padding-bottom: 10px;
- 
-}
-
-img{
-	width:60px;
-	height:60px;
-	margin: 5px 5px;
-	}
-
-.panel-info > .panel-body{
-    background:#A0A0A0;
-}
-
-.panel-primary > .panel-body {
-color:#484848; 
-background:#EFF8FB;}
-
-.gray-background{
-	background:#E0E0E0;
-}
-
-</style>
-</head>
-
-
-<div class="panel-primary">
-  <div class="panel-heading">  
-    <h1 class="panel-title text-center">Student Dashboard</h1>
+<div class="row">
+  <div class="col-lg-12">
+    <h1>Student Dashboard</h1>
   </div>
 </div>
-<div class="panel-body gray-background">
-<div class="container-fluid">
-<div class="row" style="margin-top:20px">
-  <div class="col-md-12">
-    <div class="panel-info">
-      <div class="panel-heading">
-        <h2 class="panel-title text-center">Classes</h2>
-      </div>         
-      <div class="panel-body"> 
-        <div class="panel-primary padded">
-          <div class="panel-heading">
-          </div>        
-          <div class="panel-body">
-            <div class="container-fluid">
-            <div class="row">
-                 <div class="col-xs-2">
-                  <img src="../res/earth.png" alt="earth">
-                </div>
-                <div class="col-xs-8">  
-                  <h1>Kindergarten</h1>
-                </div>
-                <div class="col-xs-2">
-                  <div class="row text-right" style="margin-top:10px">
-                    <a href="#" class="btn btn-primary" >Enter this class</a>              
-                  </div>  
-                </div>
-              </div>
-            </div>
-          </div>
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h1 class="panel-title">Module (Level) 1 or Kindergarten</h1>
+  </div>
+  <div class="panel-body">
+    <div class="col-md-12">
+      <div class="list-group">
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/earth.png" style="height:4em;width:4em;float:left;" alt="Earth">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
         </div>
-        <div class="panel-primary padded">
-          <div class="panel-heading">
-          </div>        
-          <div class="panel-body">
-            <div class="container-fluid">
-            <div class="row">
-                 <div class="col-xs-2">
-                  <img src="../res/mars.png" alt="mars">
-                </div>
-                <div class="col-xs-8">  
-                  <h1>First Grade</h1>
-                </div>
-                <div class="col-xs-2">
-                  <div class="row text-right" style="margin-top:10px">
-                    <a href="#" class="btn btn-primary" >Enter this class</a>              
-                  </div>  
-                </div>
-              </div>
-            </div>
-          </div>
+      </a>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/mars.png" style="height:4em;width:4em;float:left;" alt="Mars">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
         </div>
-         <div class="panel-primary padded">
-          <div class="panel-heading">
-          </div>        
-          <div class="panel-body">
-            <div class="container-fluid">
-            <div class="row">
-                 <div class="col-xs-2">
-                  <img src="../res/jupiter.png" alt="jupiter">
-                </div>
-                <div class="col-xs-8">  
-                  <h1>Second Grade</h1>
-                </div>
-                <div class="col-xs-2">
-                  <div class="row text-right" style="margin-top:10px">
-                    <a href="#" class="btn btn-primary" >Enter this class</a>              
-                  </div>  
-                </div>
-              </div>
-            </div>
-          </div>
+      </a>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/jupiter.png" style="height:4em;width:4em;float:left;" alt="Jupiter">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
         </div>
-        <div class="panel-primary padded">
-          <div class="panel-heading">
-          </div>        
-          <div class="panel-body">
-            <div class="container-fluid">
-            <div class="row">
-                 <div class="col-xs-2">
-                  <img src="../res/neptune.png" alt="neptune">
-                </div>
-                <div class="col-xs-8">  
-                  <h1>Third Grade</h1>
-                </div>
-                <div class="col-xs-2">
-                  <div class="row text-right" style="margin-top:10px">
-                    <a href="#" class="btn btn-primary" >Enter this class</a>              
-                  </div>  
-                </div>
-              </div>
-            </div>
-          </div>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/neptune.png" style="height:4em;width:4em;float:left;" alt="Neptune">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
         </div>
+      </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h1 class="panel-title">Module (Level) 2 or 1st Grade</h1>
+  </div>
+  <div class="panel-body">
+    <div class="col-md-12">
+      <div class="list-group">
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/earth.png" style="height:4em;width:4em;float:left;" alt="Earth">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      </a>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/mars.png" style="height:4em;width:4em;float:left;" alt="Mars">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      </a>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/jupiter.png" style="height:4em;width:4em;float:left;" alt="Jupiter">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/neptune.png" style="height:4em;width:4em;float:left;" alt="Neptune">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      </a>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h1 class="panel-title">Module (Level) 3 or 2nd Grade</h1>
+  </div>
+  <div class="panel-body">
+    <div class="col-md-12">
+      <div class="list-group">
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/earth.png" style="height:4em;width:4em;float:left;" alt="Earth">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      </a>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/mars.png" style="height:4em;width:4em;float:left;" alt="Mars">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      </a>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/jupiter.png" style="height:4em;width:4em;float:left;" alt="Jupiter">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/neptune.png" style="height:4em;width:4em;float:left;" alt="Neptune">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      </a>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="panel panel-primary">
+  <div class="panel-heading">
+    <h1 class="panel-title">Module (Level) 4 or 3rd Grade</h1>
+  </div>
+  <div class="panel-body">
+    <div class="col-md-12">
+      <div class="list-group">
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/earth.png" style="height:4em;width:4em;float:left;" alt="Earth">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      </a>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/mars.png" style="height:4em;width:4em;float:left;" alt="Mars">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      </a>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/jupiter.png" style="height:4em;width:4em;float:left;" alt="Jupiter">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      <a href="#" class="list-group-item">
+        <div>
+          <img src="../res/neptune.png" style="height:4em;width:4em;float:left;" alt="Neptune">
+          <h4><u>Mission Title</u></h4>
+          <p>A brief description of the mission can go here.</p>
+        </div>
+      </a>
+      </div>
+    </div>
+  </div>
+</div>
