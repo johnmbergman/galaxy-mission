e48ecf6a30ebce908e1ec92645b0af307dfcff90
Shaun Fyffe <shaunfyffe@gmail.com>
2015-03-27 21:46:14 -0400

    Account Settings - Parent
    Rough version of Account / Settings page. Will be accessible via the
    Parent Dashboard. Will update links shortly.

---
+++ src/views/parent-settings.php
@@ -0,0 +1,37 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-12">
+      <div class="panel-primary">
+        <div class="panel-heading">
+          <h3 class="panel-title">Account Settings</h3>
+        </div>
+        <div class="panel-body">
+          <div class="col-sm-2 col-sm-offset-2">
+            <div class="row">
+              <a href="https://www.galaxymission.com/parent-settings">Overview</a>
+            </div>
+            <div class="row">
+              <a href="https://www.galaxymission.com/profile">Account Information</a>
+            </div>
+            <div class="row">
+              <a href="#">Student Information</a>
+            </div>
+            <div class="row">
+              <a href="#">Return to Parent Dashboard</a>
+            </div>
+          </div>
+          <div class="col-sm-8">
+            <div class="row">
+              <a href="https://www.galaxymission.com/profile">Account Information</a>
+              <p>Update your personal information, including your email address and password.</p>
+            </div>
+            <div class="row">
+              <a href="https://www.galaxymission.com/student-management">Student Information</a>
+              <p>Update your child's information, including his or her grade level and school name</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
