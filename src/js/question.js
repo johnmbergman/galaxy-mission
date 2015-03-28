52f8fb1b5f7bc0e1cf3a1560a8355d3fe8e280df
jenniferSteadman <awdx111@Jen.local>
2015-03-28 16:56:24 -0400

    question form updated

--- src/js/question.js
+++ src/js/question.js
@@ -1,68 +1,26 @@
-<script type = "text/javascript">
+// src/js/question.js
+//Question Form's Javascript
+//Description: Javascript with functions for the src/views/question.php html code
+//Date: 3/17/2015
+//Author: Jennifer Steadman
+//
 
-//overided background from css
-function startFunction()
+//Functions for Question Form
+//Right now set up to display a string already set here
+//When complete the string will be sent from the server from the database
+var newQuestions = " 1 - 2 - ? - 4 - 5 ";
+var myNewElement = document.createElement("p");
+var myText = document.createTextNode(newQuestions);
+myNewElement.appendChild(myText);
+document.getElementById("div1").appendChild(myNewElement);
+
+  
+//Function is called when you input an answer and press enter on the question page
+//shows the number you submitted once you enter the answer then press enter button
+//When complete this answer will be sent to the server to input into database
+function submitFunction()
 {
-document.body.style.background = "#9f948e";
-
-
-//creates p node 
-//creates text node
-//places text into child node
-//gets elment id=div1 and places text into p
-	var newQuestions = " 3 + 4 = ";
- 	var myNewElement = document.createElement("p");
- 	var myText = document.createTextNode(newQuestions);
- 	myNewElement.appendChild(myText);
- 	document.getElementById("div1").appendChild(myNewElement);
- 	
- //access to div2 element to change styles	
- 	myNewElement.style.backgroundColor = "black";
- 	myNewElement.style.paddingRight = "20px";
- 	myNewElement.style.paddingLeft = "20px";
- 	myNewElement.style.border = "5px solid white";
- 	myNewElement.style.color = "red";
- 	myNewElement.style.fontSize = "150px";
- 	myNewElement.style.position = "fixed";
- 	myNewElement.style.left = "700px";
- 	myNewElement.style.top = "500px";
- 	
- //access to div3 element to change styles	
- 	var myElement2 = document.getElementById("div3");
- 	myElement2.style.backgroundColor = "black";
- 	myElement2.style.paddingRight = "20px";
- 	myElement2.style.paddingLeft = "20px";
- 	myElement2.style.border = "5px solid white";
- 	myElement2.style.color = "red";
- 	myElement2.style.fontSize = "150px";
- 	myElement2.style.position = "fixed";
-    myElement2.style.left = "1300px";
- 	myElement2.style.top = "500px";
- 	
- 
- //access to document object FORM with name = Question to change styles
-    var que = document.Question;
-    que.style.color = "white";
-    que.style.fontSize = "40px";
-    que.style.position = "fixed";
-    que.style.left = "700px";
-    que.style.top = "750px";
-    que.style.backgroundColor = "black";
-    que.style.border = "5px solid white";
-    que.style.paddingRight = "20px";
-    que.style.paddingLeft = "20px";
-    que.style.paddingBottom = "10px";
-    
- //access to document form input box to make text in box black   
-    var queInput = document.Question.number;
-    queInput.style.color = "black";
-    
-  } 
- //shows the number you submitted later this will send the number to server instead
-    function submitFunction()
-    {
-	  var x = document.Question.number.value;
-	  document.getElementById("div2").innerHTML = "The number you entered was: " + x;	
-    }
-
-</script>
+  var x = document.Question.number.value;	  
+  document.getElementById("div2").innerHTML = "The number you entered was: " + x;
+  console.log("You Entered: " + x);	
+}
