221eeef2c87822e22d682d04a032c2a3c3d23ace
Shaun Fyffe <shaunfyffe@gmail.com>
2015-03-28 09:01:49 -0400

    Updated layout of account settings & student info
    Add side-nav bar for easier navigation

---
+++ src/views/account-settings.php
@@ -0,0 +1,34 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-12">
+      <h2>Account Settings</h2>
+      <div class="panel-primary">
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-sm-4">
+              <ul class="nav nav-pills nav-stacked">
+                <li class="active"><a href="https://www.galaxymission.com/account-settings">Overview</a></li>
+                <li class="divider"></li>
+                <li><a href="https://www.galaxymission.com/profile">Account Information</a></li>
+                <li class="divider"></li>
+                <li><a href="https://www.galaxymission.com/student-management">Student Information</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Return to Parent Dashboard</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-7 col-sm-offset-1">
+              <div class="row">
+                <a href="https://www.galaxymission.com/profile">Account Information</a>
+                <p>Update your personal information, including your email address and password.</p>
+              </div>
+              <div class="row">
+                <a href="https://www.galaxymission.com/student-management">Student Information</a>
+                <p>Update your child's information, including his or her grade level and school name.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
--- src/views/student-management.php
+++ src/views/student-management.php
@@ -1,66 +1,80 @@
-<div class="row">
-  <div class="col-md-6 col-md-offset-3">
-    <div class="text-center">
-      <h1 class="text-black">Insert Student Name's Information</h1>
-      <p class="lead text-black">Click in a field to edit information you would like to change, and then click the Save button.</p>
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-12">
+      <h2>Student Name's Information</h2>
+      <div class="panel-primary">
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-sm-4">
+              <ul class="nav nav-pills nav-stacked">
+                <li class="active"><a href="https://www.galaxymission.com/account-settings">Overview</a></li>
+                <li class="divider"></li>
+                <li><a href="https://www.galaxymission.com/profile">Account Information</a></li>
+                <li class="divider"></li>
+                <li><a href="https://www.galaxymission.com/student-management">Student Information</a></li>
+                <li class="divider"></li>
+                <li><a href="#">Return to Parent Dashboard</a></li>
+              </ul>
+            </div>
+            <div class="col-sm-8">
+              <form method="post" class="form-horizontal well">
+                <fieldset>
+                  <legend>Click in a field to edit information and then click Save.</legend>
+                  <div class="form-group">
+                    <label for="firstName" class="col-lg-4 control-label">First Name</label>
+                    <div class="col-lg-8">
+                      <input type="text" class="form-control" id="firstName" placeholder="First Name">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="lastName" class="col-lg-4 control-label">Last Name</label>
+                    <div class="col-lg-8">
+                      <input type="text" class="form-control" id="lastName" placeholder="Last Name">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="gender" class="col-lg-4 control-label">Gender</label>
+                    <div class="col-lg-8">
+                      <select class="form-control" id="gender">
+                        <option>Male</option>
+                        <option>Female</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="grade" class="col-lg-4 control-label">Grade Level</label>
+                    <div class="col-lg-8">
+                      <select class="form-control" id="grade">
+                        <option>Kindergarten</option>
+                        <option>1st Grade</option>
+                        <option>2nd Grade</option>
+                        <option>3rd Grade</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="schoolName" class="col-lg-4 control-label">School Name</label>
+                    <div class="col-lg-8">
+                      <input type="text" class="form-control" id="schoolName" placeholder="School Name (optional)">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="teacherName" class="col-lg-4 control-label">Teacher Name</label>
+                    <div class="col-lg-8">
+                      <input type="text" class="form-control" id="teacherName" placeholder="Teacher Name (optional)">
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <div class="col-lg-8 col-lg-offset-4">
+                      <button type="submit" class="btn btn-primary">Save</button>
+                    </div>
+                  </div>
+                </fieldset>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-<div class="row">
-  <div class="col-md-6 col-md-offset-3">
-    <form method="post" class="form-horizontal well">
-      <fieldset>
-      <legend>Student Name's Information</legend>
-        <div class="form-group">
-          <label for="firstName" class="col-lg-4 control-label">First Name</label>
-          <div class="col-lg-8">
-            <input type="text" class="form-control" id="firstName" placeholder="First Name">
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="lastName" class="col-lg-4 control-label">Last Name</label>
-          <div class="col-lg-8">
-            <input type="text" class="form-control" id="lastName" placeholder="Last Name">
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="gender" class="col-lg-4 control-label">Gender</label>
-          <div class="col-lg-8">
-            <select class="form-control" id="gender">
-              <option>Male</option>
-              <option>Female</option>
-            </select>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="grade" class="col-lg-4 control-label">Grade Level</label>
-          <div class="col-lg-8">
-            <select class="form-control" id="grade">
-              <option>Kindergarten</option>
-              <option>1st Grade</option>
-              <option>2nd Grade</option>
-              <option>3rd Grade</option>
-            </select>
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="schoolName" class="col-lg-4 control-label">School Name</label>
-          <div class="col-lg-8">
-            <input type="text" class="form-control" id="schoolName" placeholder="School Name (optional)">
-          </div>
-        </div>
-        <div class="form-group">
-          <label for="teacherName" class="col-lg-4 control-label">Teacher Name</label>
-          <div class="col-lg-8">
-            <input type="text" class="form-control" id="teacherName" placeholder="Teacher Name (optional)">
-          </div>
-        </div>
-        <div class="form-group">
-          <div class="col-lg-8 col-lg-offset-4">
-            <button type="submit" class="btn btn-primary">Save</button>
-          </div>
-        </div>
-      </fieldset>
-    </form>
   </div>
 </div>
