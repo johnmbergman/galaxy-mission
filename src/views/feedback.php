daf9732ae35a255a99387862794ac4dd92c29327
Shaun Fyffe <shaunfyffe@gmail.com>
2015-04-09 23:21:48 -0400

    Added feedback.php
    Added feedback.php

---
+++ src/views/feedback.php
@@ -0,0 +1,55 @@
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-sm-10 col-sm-offset-1">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <form class="form-horizontal">
+            <fieldset>
+              <legend>Send Us Your Feedback</legend>
+              <div class="form-group">
+                <label for="inputName" class="col-sm-2 control-label">Name</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputName" placeholder="Name">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputEmail" placeholder="Email">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="select" class="col-sm-2 control-label">Reason</label>
+                <div class="col-sm-10">
+                  <select class="form-control" id="select">
+                    <option>I'd like to report a bug in Galaxy Mission</option>
+                    <option>I love Galaxy Mission! I'd like to submit a testimonial.</option>
+                    <option>Other (Please explain below)</option>
+                  </select>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="inputSubject" class="col-sm-2 control-label">Subject</label>
+                <div class="col-sm-10">
+                  <input type="text" class="form-control" id="inputSubject" placeholder="Enter Your Subject Here">
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="textArea" class="col-sm-2 control-label">Textarea</label>
+                <div class="col-sm-10">
+                  <textarea class="form-control" rows="5" id="textArea"></textarea>
+                </div>
+              </div>
+              <div class="form-group">
+                <div class="col-sm-10 col-sm-offset-2">
+                  <button type="reset" class="btn btn-default">Cancel</button>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </div>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
